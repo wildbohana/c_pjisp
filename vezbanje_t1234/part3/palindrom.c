c203c632d674dada1b7ce47547d735fe1458c4d8
wildbohana <83251325+wildbohana@users.noreply.github.com>
2021-04-29 02:44:55 +0200

    strlen

---
+++ vezbanje_t1234/part3/palindrom.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_SIZE 101
+
+int main() {
+	
+	char niz[MAX_SIZE], b[MAX_SIZE];
+	int i, j;
+	
+	//unos stringa
+	printf("Unesite string: ");
+	__fpurge(stdin);
+	gets(niz);
+	
+	//smanjujemo slova
+	i = 0;
+	while(niz[i]) {
+		if (niz[i] >= 'A' && niz[i] <= 'Z')
+			niz[i] += 32;
+		i++;
+	}
+	
+	
+	
+	//izbacivanje razmaka
+	j = 0;
+	for (i = 0; i < strlen(niz); i++) {
+		if(niz[i]!= ' ' && niz[i] != '\t') {
+			b[j] = niz[i];
+			j++;
+		}
+	}
+	b[j] = '\0';
+	int nijePalindrom = 1;
+	
+	//provera da li je palindrom
+	for(i = 0, j = strlen(b) - 1; i < strlen(b)/2; i++, j--){
+		if(b[i] != b[j])
+			nijePalindrom = 0;
+	}
+	
+	if (!nijePalindrom) printf("String nije palindrom. \n");
+	else printf("String je palindrom.\n");
+	
+	return 0;
+}
